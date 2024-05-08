c53fe8a4f7acd7524e68861beab11108ad92a4b2
Zaayan325 <150224587+Zaayan325@users.noreply.github.com>
2024-05-08 18:14:06 +0500

    Blog Create Done

---
+++ app/Http/Controllers/BlogController.php
@@ -0,0 +1,87 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Blog;
+use Illuminate\Support\Facades\Validator;
+
+class BlogController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        $blogs = Blog::get();
+        return view('blog.index' ,compact('blogs'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        $data = [
+            'title'=> 'Required|min:10',
+            'comtent'=> 'Required|min:500'
+        ];
+        $validator = Validator::make($request->all(), $data);
+        if($validator->fails()){
+            return redirect()->route('')->withInput()->withErrors($validator);
+        }
+
+        if($request->hasFile('multiple_images')) {
+            $images =  $request->file('multiple_images');
+            $imagepath = [];
+
+            foreach ($images as $image){
+                $filename = date('dmY').time(). '.' .$image->getClientOriginalExtension();
+                $image->move(public_path("/uploads"), $filename);
+                $imagepath[] = $filename;
+            }
+            $data['multiple_images'] = json_encode($imagepath);
+            $create = Blog::create($data);
+        }
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(string $id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        //
+    }
+}
---
+++ database/migrations/2024_05_06_131329_create_blogs_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('blogs', function (Blueprint $table) {
+            $table->id();
+            $table->string('tilte');
+            $table->string('content');
+            $table->url('multiple_images');
+            $table->timestamp('Aproved_at');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('blogs');
+    }
+};
